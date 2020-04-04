6495c12eb16b0a35e80226362f194f07200802a3
smartpie37 <smartpie@yandex.ru>
2020-04-04 23:39:28 +0300

    Ending of the first step

---
+++ resources/views/inc/navbar.blade.php
@@ -0,0 +1,22 @@
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark mb-3">
+	<div class="container-xl">
+		<a class="navbar-brand" href="/">{{config('app.name', 'LSAPP')}}</a>
+		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExample07XL" aria-controls="navbarsExample07XL" aria-expanded="false" aria-label="Toggle navigation">
+			<span class="navbar-toggler-icon"></span>
+		</button>
+
+		<div class="collapse navbar-collapse" id="navbarsExample07XL">
+			<ul class="navbar-nav mr-auto">
+				<li class="nav-item">
+					<a class="nav-link" href="/">Home</a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link" href="/about">About</a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link" href="/services">Services</a>
+				</li>
+			</ul>
+		</div>
+	</div>
+</nav>
